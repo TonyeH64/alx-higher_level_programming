87315ad9b8c46d9d19b42f82c31b2019259c123c
TonyeH64 <harrytonye64@gmail.com>
2024-04-20 10:52:26 -0700

    0x0F-python-object_relational_mapping tasks complete

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,30 @@
-#!/user/bin/python3
-"""Module that lists all states fomr mySQL database"""
-import sys
-import mySQLdb
+#!/usr/bin/python3
 
-def list_states(username, password, database):
-    # Connect to the mySQL server
-    db = mySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+"""
+    A script that lists all states from the database hbtn_0e_0_usa
+    Username, password and database names are given as user args
+"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
     cursor = db.cursor()
 
-    # Execute the SQL query to fetch all states
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all the the rows from the query result
-    rows = cursor.fetchall()
+    data = cursor.fetchall()
 
-    # Print the results
-    for row in rows:
+    for row in data:
         print(row)
 
-    # Close the database connection
+    cursor.close()
     db.close()
-
-    # Example usage
-    if __name__ == '__main__':
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-
-        list_states(username, password, database) 
